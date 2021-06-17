c76061637b336cf1e351f1d807d7b24f15c36844
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-17 14:27:34 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,5 +5,5 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
-#sfdx force:auth:logout --targetusername %USERNAME% -p
+#sfdx force:auth:logout --targetusername %USERNAME% -p   sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
+sfdx force:auth:logout --targetusername% USERNAME% -p & sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --instanceurl "https://test.salesforce.com" --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci --jwtkeyfile $SFDC_SERVER_KEY & sfdx force:source:deploy -u $SFDC_PROD_CLIENTID -p "src" -w 5 \
