a7eb876479b1298e2a34e015d0cdcf9c8e49d944
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-17 22:33:55 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,6 @@
-#Get the private key from the environment variable
-echo "Setting up SANDBOX Connection..."
+echo "Logging into Salesforce Org"
 mkdir JWT
-echo $SFDC_SERVER_KEY| base64 -d > JWT/server.key
+echo $SFDC_SERVER_KEY | base64 -di > JWT/server.key
 
-# Authenticate to salesforce
-echo "Authenticating..."
-sfdx force:auth:logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
+echo "Authenticating org"
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
