00323b945ebc90029a7c2bae2506f9b917aa6a8a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-18 18:32:49 -0400

    Testing a test

--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,3 @@
-#sfdx force:auth:web:login -a $SFDC_PROD_USER -r https://test.salesforce.com/ -s
+sfdx force:auth:web:login -a $SFDC_PROD_USER -r https://test.salesforce.com/ -s
 sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:apex:test:run --testlevel RunSpecifiedTests -r "AMICAR_ChecklistGetJSONTest" --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}
+sfdx force:apex:test:run -t "AMICAR_ChecklistGetJSONTest" -r human
