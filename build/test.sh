4b494e93b3522e3478f008fec2ab2ebd9236e26e
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-18 19:34:08 -0400

    t

--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,3 @@
 sfdx force:auth:web:login -a $SFDC_PROD_USER -r https://test.salesforce.com/ -s
 sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:apex:test:run -t "AMICAR_ChecklistGetJSONTest" -r human
+sfdx force:apex:test:run -t "AMICAR_ChecklistGetJSONTest" -d C:\Users\Equipo\Documents\The Welkin Suite\Projects\AmicarPruebaCI\AmicarPruebaCI\build\test-result -r human
