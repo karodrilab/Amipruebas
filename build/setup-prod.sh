08bc8004666a52143a94487237324b41cbcbd6b8
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-17 23:34:32 -0400

    1.6

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -4,4 +4,4 @@ mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -5,4 +5,4 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a 
+sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a 
