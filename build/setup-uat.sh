bf264596a2dbfd59b224ee61b9fb0eaabdd8c75a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-17 22:48:14 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -5,4 +5,4 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
+sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a 
