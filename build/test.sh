00634689d1915709ee145ef33381b718ab90ce61
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-17 23:50:47 -0400

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,3 @@
-sfdx force:auth:web:login -a $SFDC_PROD_USER  -r https://test.salesforce.com/ -s
+sfdx force:auth:web:login -a $SFDC_UAT_USER  -r https://test.salesforce.com/ -s
 sfdx force:source:push -u ${CIRCLE_BRANCH}
 sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}
