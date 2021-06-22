b44d493ebd8cef2d52ddcd0e9e90454cdec0542e
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 15:46:54 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -1,6 +1,6 @@
 #Get the private key from the environment variable
 echo "Setting up SANDBOX Connection..."
-mkdir JWT
+#mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
