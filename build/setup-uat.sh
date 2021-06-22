46c01655c37c8c1271f03d843ee23339fd7f080a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 16:37:15 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -1,6 +1,8 @@
 # Get the private key from the environment variable
-echo "Logging into Salesforce Org"
-mkdir JWT
+echo "Logging into Salesforce OrgUAT"
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
-echo "Authenticating org"
+mkdir JWT
+echo "Authenticating orgUAT"
 sfdx force:auth:logout --targetusername $SFDC_UAT_USER -p & sfdx force:auth:jwt:grant -i $SFDC_UAT_CLIENTID -f JWT/server.key -u $SFDC_UAT_USER -a DevHubUAT
+echo dir
+del JWT/server.key
