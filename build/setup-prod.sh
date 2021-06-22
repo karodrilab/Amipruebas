ee0dcc28eab500ed74d0e2f711cc4b759166780a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 16:14:17 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -4,4 +4,4 @@ mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f JWT/server.key -u $SFDC_PROD_USER -a DevHubProd
+sfdx force:auth:logout --targetusername $SFDC_PROD_USER -p & sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f JWT/server.key -u $SFDC_PROD_USER -a DevHubProd
