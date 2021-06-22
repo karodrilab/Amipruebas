278494035f7dcbae83fc302edd8fcda8574e5338
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 17:23:25 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -6,4 +6,4 @@ echo "hola"
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 echo "Authenticating orgUAT"
 #sfdx force:auth:logout --targetusername $SFDC_PROD_USER -p & 
-sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f JWT/server.key -u $SFDC_UAT_USER -a DevHubUAT
+sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f JWT/server.key -u $SFDC_UAT_USER -a DevHubUAT --instanceurl "https://test.salesforce.com"
