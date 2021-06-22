d73d19e1db2ad05f5b7e8cef1efdac92ad241409
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 16:31:52 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,8 +1,7 @@
 # Get the private key from the environment variable
 echo "Logging into Salesforce Org"
-mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
-
+mkdir JWT
 echo "Authenticating org"
 sfdx force:auth:logout --targetusername $SFDC_PROD_USER -p & sfdx force:auth:jwt:grant -i $SFDC_PROD_CLIENTID -f JWT/server.key -u $SFDC_PROD_USER -a DevHubProd
 del JWT/server.key
