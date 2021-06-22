ffea967bc113ef7ad455b74af02aa6b8d3b5742b
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-22 15:27:29 -0400

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,4 +1,5 @@
 #sfdx force:auth:web:login -a $SFDC_PROD_USER -r https://test.salesforce.com/ -s
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:apex:test:run -n "AMICAR_ChecklistGetJSONTest" -r human -u $SFDC_PROD_USER
+sfdx force:apex:test:run -n "AMICAR_ChecklistGetJSONTest" -r human 
+#-u $SFDC_PROD_USER
 sfdx force:org:list
