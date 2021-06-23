3ee8ab22c8af9e07338cd810c3d33b3d4cf0d302
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 01:08:29 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -1,7 +1,9 @@
-# Get the private key from the environment variable
-echo "Logging into Salesforce Org"
+#Get the private key from the environment variable
+echo "Setting up SANDBOX Connection..."
 #mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
-echo "Authenticating org"
-sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a DevHub
+# Authenticate to salesforce
+echo "Authenticating uat..."
+#sfdx force:auth:logout --targetusername $SFDC_PROD_USER -p 
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID -u --username $SFDC_PROD_USER -f --jwtkeyfile $SFDC_SERVER_KEY --setdefaultdevhubusername -a DevhubQA
