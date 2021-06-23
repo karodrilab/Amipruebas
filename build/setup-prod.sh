95ad723a371c39c01814c2efd94b264a81a66fc8
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 18:14:41 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -8,4 +8,5 @@ echo "Authenticating org"
 #PRUEBACI
 echo "Conectar PruebasCi"
 #sfdx auth:jwt:grant --clientid 3MVG9eQyYZ1h89HcPBQTxIzUhVbC9BLVh_W8x4VdZ4xjNT0A5xTjv0aKXIy4k0bTvv.QUNRcA5Ug0DjlROf4w --jwtkeyfile JWT/server.key --username consultor_force+amicar@nectia.com.pruebasci --instanceurl https://test.salesforce.com
+sfdx alias:set my-hub-orgPruebasCI=consultor_force+amicar@nectia.com.pruebasci
 sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
