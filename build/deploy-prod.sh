166182c7f947c5f7edb4483eb0a07ebb69f4c416
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 19:49:45 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -2,10 +2,10 @@
 echo "Converting to MDAPI format..."
 sfdx force:org:list
 mkdir packageRoles
-sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI -k /home/circleci/project/packageRoles/package.xml 
-sfdx force: mdapi:convert -d deploy_prod -r packageRoles
-sfdx force:mdapi:retrieve:report -u consultor_force+amicar@nectia.com.pruebasci
-#sfdx force:mdapi:deploy -c -f packageRoles.zip -u my-hub-Dodoria -w 10
+#sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI -k /home/circleci/project/packageRoles/package.xml 
+#sfdx force: mdapi:convert -d deploy_prod -r packageRoles
+#sfdx force:mdapi:retrieve:report -u consultor_force+amicar@nectia.com.pruebasci
+sfdx force:mdapi:deploy -c -d /home/circleci/project -u my-hub-Dodoria -w 10
 
 #r: donde se guardara el archivo.
 #-k listado de componentes 
