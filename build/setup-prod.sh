0f2b95dde03d5b4937918ae6b053d6548009a42b
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 15:35:22 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,4 +5,4 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 echo "Authenticating org"
 #sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --setdefaultdevhubusername
 #sfdx force: auth: logout --targetusername $SFDC_UAT_USER -p sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER
-sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://amicar--sbxdodoria.my.salesforce.com
+sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
