06ff3379da3e22e7efcbc05f6eb34202f46006a0
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 15:31:25 -0400

    Commit Test

---
+++ build/create-scratch-org.sh
@@ -0,0 +1,8 @@
+# Get the private key from the environment variable
+echo "Setting up DevHub Connection..."
+mkdir JWT
+echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
+
+# Authenticate to salesforce
+echo "Authenticating..."
+sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
---
+++ build/setup-prod.sh
@@ -0,0 +1,7 @@
+# Get the private key from the environment variable
+echo "Logging into Salesforce Org"
+mkdir JWT
+echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
+
+echo "Authenticating org"
+sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
