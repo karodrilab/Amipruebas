dbcb71c77cb1a9af93a8a10dd9cc7fac0f664e6a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 14:18:10 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,29 +1,7 @@
-# Get the private key from the environment variable
-#echo "Logging into Salesforce Org"
-#mkdir JWT
-#echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
-#echo "Authenticating org"
-#sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --setdefaultdevhubusername
-#sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
-#Create a scratch org
-echo "Creating the Scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json --setalias Amipruebas --durationd days 30 --targetdevhubusername $SFDC_PROD_USER
-#sfdx force:org:create -f project-scratch-def.json -a Amipruebas
-#Push the code to your new scratch org & run all tests
-echo "Pushing code to Scratch Org..."
-sfdx force:org:list
-sfdx force:source:push -u ${CIRCLE_BRANCH}
-echo "Running all tests on Scratch Org..."
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername $SFDC_PROD_USER
-
-# Get the private key from the environment variable
+#Get the private key from the environment variable
 echo "Logging into Salesforce Org"
 #mkdir JWT
 echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 echo "Authenticating org"
 #sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --setdefaultdevhubusername
-#sfdx force:auth:logout --targetusername $SFDC_PROD_USER -p 
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
-
-
-
+sfdx force: auth: logout --targetusername $SFDC_PROD_USER -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER
