1453eb9a02f67ee0ddcc7db1b4c74cc4e3b6060f
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 18:02:24 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,15 +1,17 @@
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
-sfdx force:mdapi:retrieve -r /packageRoles.zip -u my-hub-orgPruebasCI -k /usr/local/lib/sfdx/packageClasses.xml
+sfdx force:org:list
+sfdx force:mdapi:retrieve -r /packageRoles.zip -u my-hub-orgPruebasCI -k /packageRoles.xml
 sfdx force:mdapi:deploy -c -f /packageRoles.zip -u my-hub-org2 -w 10
 
 #r: donde se guardara el archivo.
 #-k listado de componentes 
-#
+#-c Valida
+#-f dondese encuentra el zip
 
-sfdx force: mdapi:convert -d deploy_prod -r mdapipackage /
+#sfdx force: mdapi:convert -d deploy_prod -r mdapipackage /
 #sfdx force:source:convert -d deploy_prod -r src
 #Deploy to prod & run all tests
-echo "Deploying to Desa & running all tests..."
-sfdx force:mdapi:deploy -u consultor_force+amicar@nectia.com.pruebasci -d deploy_prod/ -w -1 -l RunAllTestsInOrg
+#echo "Deploying to Desa & running all tests..."
+#sfdx force:mdapi:deploy -u consultor_force+amicar@nectia.com.pruebasci -d deploy_prod/ -w -1 -l RunAllTestsInOrg
 #sfdx force:source:deploy -x C:/Users/karen/Documents/GitHub/amicarci/src/main/default/package.xml -g
