5b5fbffd1cd20ab5e5f58d732379e8f7716c1f47
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 17:58:53 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -1,5 +1,12 @@
 #Convert to MDAPI format for deployment to prod
 echo "Converting to MDAPI format..."
+sfdx force:mdapi:retrieve -r /packageRoles.zip -u my-hub-orgPruebasCI -k /usr/local/lib/sfdx/packageClasses.xml
+sfdx force:mdapi:deploy -c -f /packageRoles.zip -u my-hub-org2 -w 10
+
+#r: donde se guardara el archivo.
+#-k listado de componentes 
+#
+
 sfdx force: mdapi:convert -d deploy_prod -r mdapipackage /
 #sfdx force:source:convert -d deploy_prod -r src
 #Deploy to prod & run all tests
