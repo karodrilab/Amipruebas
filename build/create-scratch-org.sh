bd415d6d4849851795580c4f624abf09424c1247
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 16:45:26 -0400

    Update create-scratch-org.sh

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -1,8 +1,5 @@
 # Get the private key from the environment variable
 echo "Setting up DevHub Connection..."
-mkdir JWT
-echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
-
-# Authenticate to salesforce
-echo "Authenticating..."
-sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.pruebasci
+echo "Authenticating org"
+echo "Conectar PruebasCi"
+sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
