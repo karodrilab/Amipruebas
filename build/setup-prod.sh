1b37fe244bada71c6f7cfea35efdf08da3df4d62
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 16:00:50 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -1,4 +1,5 @@
 #Get the private key from the environment variable
 echo "Logging into Salesforce Org"
 echo "Authenticating org"
-sfdx auth:jwt:grant --clientid 3MVG9Ccwq.TeycMai5QnOzXW5fUZzNLJyLbXgpcAlGHInayKphJPiC11LbCdYViBppKSSO5Ee3xOiLqRkBjEE --jwtkeyfile JWT/server.key --username consultor_force+amicar@nectia.com.sbxdodoria --instanceurl https://test.salesforce.com
+#sfdx auth:jwt:grant --clientid 3MVG9Ccwq.TeycMai5QnOzXW5fUZzNLJyLbXgpcAlGHInayKphJPiC11LbCdYViBppKSSO5Ee3xOiLqRkBjEE --jwtkeyfile JWT/server.key --username consultor_force+amicar@nectia.com.sbxdodoria --instanceurl https://test.salesforce.com
+#sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
