7de46faf8749498a4115a73ac6c231ec0d473f2d
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 18:35:50 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -2,6 +2,7 @@
 echo "Logging into Salesforce Org"
 echo "Authenticating orgqa"
 #PRUEBACI
-echo "Conectar PruebasCi"
+echo "Conectar Dodoria"
 #sfdx auth:jwt:grant --clientid 3MVG9eQyYZ1h89HcPBQTxIzUhVbC9BLVh_W8x4VdZ4xjNT0A5xTjv0aKXIy4k0bTvv.QUNRcA5Ug0DjlROf4w --jwtkeyfile JWT/server.key --username consultor_force+amicar@nectia.com.pruebasci --instanceurl https://test.salesforce.com
-sfdx auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_UAT_USER --instanceurl https://test.salesforce.com
+sfdx auth:web:login --setalias  --instanceurl https://test.salesforce.com
+sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
