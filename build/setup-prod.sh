42c029e2474ad7ecd68922d44e95da35ce976ea1
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 10:03:35 -0400

    Update setup-prod.sh

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -7,12 +7,12 @@
 #sfdx force: auth: logout --targetusername %USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
 #Create a scratch org
 echo "Creating the Scratch Org..."
-sfdx force:org:create -f . config/project-scratch-def.json --setalias Amipruebas --durationd days 30 --targetdevhubusername $SFDC_PROD_USER
+sfdx force:org:create -f config/project-scratch-def.json --setalias Amipruebas --durationd days 30 --targetdevhubusername $SFDC_PROD_USER
 #sfdx force:org:create -f project-scratch-def.json -a Amipruebas
 #Push the code to your new scratch org & run all tests
 echo "Pushing code to Scratch Org..."
 sfdx force:org:list
-sfdx force:source:push -u UAT
+sfdx force:source:push -u ${CIRCLE_BRANCH}
 echo "Running all tests on Scratch Org..."
 sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername $SFDC_PROD_USER
 
