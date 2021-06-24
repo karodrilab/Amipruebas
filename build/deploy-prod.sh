dbff21f7a785308d928dbf100b33e05dad220788
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-23 20:30:21 -0400

    Update deploy-prod.sh

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -6,7 +6,7 @@ echo "Command - sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI
 #sfdx force:source:retrieve -r packageRoles -u my-hub-orgPruebasCI -k ./package.xml
 sfdx force:mdapi:retrieve -r packageRoles -u my-hub-orgPruebasCI -k src/package.xml
 #sfdx force: mdapi:convert -d deploy_prod -r packageRoles
-sfdx force:mdapi:retrieve:report -u consultor_force+amicar@nectia.com.pruebasci
+sfdx force:mdapi:retrieve:report
 #sfdx force:mdapi:deploy -c -d packageRoles -u my-hub-Dodoria -w 10
 
 #r: donde se guardara el archivo.
